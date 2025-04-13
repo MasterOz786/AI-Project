818263925dd0414c58904a473a8bd38576c56d5a
MasterOz786 <i222434@nu.edu.pk>
2025-04-14 01:03:48 +0500

    setup: initial structure of chaapa

---
+++ audio/config.py
@@ -0,0 +1,3 @@
+
+TRAINING_FILES_PATH = '/home/moz/Desktop/22i - 2434/Semester 6/ai/AI-Project/audio/features/'
+TESS_ORIGINAL_FOLDER_PATH = '/home/moz/Desktop/22i - 2434/Semester 6/ai/AI-Project/audio/TESS_Toronto_emotional_speech_set_data/'
